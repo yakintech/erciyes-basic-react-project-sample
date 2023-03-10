6dc15d2a8fee3c66e3543814291f462c0d6f314f
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-03-10 19:09:58 +0300

    detail page

--- src/views/category/Detail.jsx
+++ src/views/category/Detail.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { Spin } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function Detail() {
-  return (
-    <div>Detail Page</div>
+
+  const [categoryDetail, setcategoryDetail] = useState({});
+  const [loading, setloading] = useState(true);
+
+  let { id } = useParams();
+
+  useEffect(() => {
+
+
+    axios.get('https://northwind.vercel.app/api/categories/' + id)
+      .then(res => {
+        setcategoryDetail(res.data);
+        setloading(false)
+      })
+  }, [])
+
+
+  return (<>
+    <Spin tip="Loading..." spinning={loading}>
+      <h1>Id: {categoryDetail.id} </h1>
+      <h1>Name: {categoryDetail.name}</h1>
+      <h1>Description: {categoryDetail.description} </h1>
+
+    </Spin>
+
+  </>
   )
 }
 
